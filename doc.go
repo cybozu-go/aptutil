582463fdb4475b16d5e5ba5de46d2cbfa5419631
ymmt2005 <ymmt2005@gmail.com>
2016-07-14 18:17:16 +0900

    add top-level doc.go for godoc.org

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Package aptutil consists of these sub packages.
+
+    apt     - APT repository utilities.
+    cacher  - go-apt-cacher logics.
+    mirror  - go-apt-mirror logics.
+    cmd     - main functions.
+*/
+package aptutil
